07fd8e8197f893748618a0564a048eb2996145d3
Yogansh Gharti <yogansh.yg@gmail.com>
2026-04-04 10:19:15 +0530

    Time: 4 ms (5.09%) | Memory: 27.3 MB (17.59%) - LeetSync

---
+++ 189-rotate-array/rotate-array.c
@@ -0,0 +1,22 @@
+void reverse(int* nums, int start, int end) {
+    while(start < end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void rotate(int* nums, int numsSize, int k) {
+    k = k % numsSize;
+
+    // step 1: reverse whole array
+    reverse(nums, 0, numsSize - 1);
+
+    // step 2: reverse first k elements
+    reverse(nums, 0, k - 1);
+
+    // step 3: reverse remaining elements
+    reverse(nums, k, numsSize - 1);
+}
