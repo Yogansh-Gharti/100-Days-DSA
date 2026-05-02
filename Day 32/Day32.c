3ab8bcec52111314f6d45a63ae1667480a452d59
Yogansh Gharti <yogansh.yg@gmail.com>
2026-05-02 09:51:41 +0530

    Add files via upload

---
+++ Day 32/Day32.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+void push(int value)
+{
+    if(top == MAX-1)
+    {
+        printf("Stack Overflow\n");
+        return;
+    }
+    top++;
+    stack[top] = value;
+}
+
+void pop()
+{
+    if(top == -1)
+    {
+        printf("Stack Underflow\n");
+        return;
+    }
+    top--;
+}
+
+void display()
+{
+    for(int i = top; i >= 0; i--)
+    {
+        printf("%d ", stack[i]);
+    }
+}
+
+int main()
+{
+    int n, m, value;
+
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &value);
+        push(value);
+    }
+
+    scanf("%d", &m);
+
+    for(int i = 0; i < m; i++)
+    {
+        pop();
+    }
+
+    display();
+
+    return 0;
+}
